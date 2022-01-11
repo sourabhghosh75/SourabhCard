967a31d805456063c91624566ff4525eb9fe0887
Sourabh Ghosh <sourabh.ghosh75@gmail.com>
2022-01-11 12:51:04 +0530

    Initial Commit

---
+++ SourabhCard/SourabhCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourabhCardApp.swift
+//  SourabhCard
+//
+//  Created by Sourabh Ghosh on 11/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SourabhCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
